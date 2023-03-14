81f95fa744c67653c28ec5580265267e9ef95ee3
Deniz_Aktas <deniz.aktas@chasacademy.se>
2023-03-14 10:01:28 +0100

    Main img

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,21 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import stormtrooper from './assets/Stormtrooper.jpg';
+
+const MainImgcontainer = styled.div`
+max-width: 50vw;
+grid-area: 2 / 1 / span 2 / span 2;
+`;
+
+
+
+
 
 
 function Home() {
 
     return (
-        <p>Home</p>
+        <p>Welcome to the person who did it!</p>
     );
 }
 
